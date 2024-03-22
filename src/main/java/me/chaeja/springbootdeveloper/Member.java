dbb089ecabb0f8d867a9d78f6389387cbca02c3e
jeongahChae <chae4582@gmail.com>
2024-03-22 16:15:01 +0900

    DB, ORM, JPA

--- src/main/java/me/chaeja/springbootdeveloper/Member.java
+++ src/main/java/me/chaeja/springbootdeveloper/Member.java
@@ -10,14 +10,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)	//기본 생성자
 @AllArgsConstructor
 @Getter
 @Entity
 public class Member {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	//기본키 생성 방식 결정, IDENTITY: 기본키 생성을 DB에 위임
 	@Column(name = "id", updatable = false)
 	private Long id;
 	
