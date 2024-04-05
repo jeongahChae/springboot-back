dbf3c31e64d3874b8508b92e0dca20584bc6b9ef
jeongahChae <chae4582@gmail.com>
2024-04-05 10:58:06 +0900

    블로그 글 수정 기능 추가

--- springboot-blog/src/main/java/me/chae/blog/controller/BlogViewController.java
+++ springboot-blog/src/main/java/me/chae/blog/controller/BlogViewController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import me.chae.blog.domain.Article;
@@ -33,5 +34,17 @@ public class BlogViewController {
 		model.addAttribute("article", new ArticleViewResponse(article));
 		
 		return "article";
+	}
+	
+	@GetMapping("/new-article")
+	public String newArticle(@RequestParam(required = false, name = "id") Long id, Model model) {
+		if(id == null) {	// id가 없으면 생성
+			model.addAttribute("article", new ArticleViewResponse());
+		} else {			// id가 있으면 수정
+			Article article = blogService.findById(id);
+			model.addAttribute("article", new ArticleViewResponse(article));
+		}
+		
+		return "newArticle";
 	}
 }
