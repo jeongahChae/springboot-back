2dd67e1b04b850226d59099df24dd77d9ddda0a8
jeongahChae <chae4582@gmail.com>
2024-04-05 09:44:36 +0900

    블로그 글 목록 view 만들기

---
+++ springboot-blog/src/main/java/me/chae/blog/controller/BlogViewController.java
@@ -0,0 +1,26 @@
+package me.chae.blog.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import me.chae.blog.dto.ArticleListViewResponse;
+import me.chae.blog.service.BlogService;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+	private final BlogService blogService;
+	
+	@GetMapping("/articles")
+	public String getArticles(Model model) {
+		List<ArticleListViewResponse> articles = blogService.findAll().stream().map(ArticleListViewResponse::new).toList();
+		model.addAttribute("articles", articles);	// 블로그 글 리스트 저장
+		
+		return "articleList";	//articleList.html라는 뷰 조회
+	}
+}
